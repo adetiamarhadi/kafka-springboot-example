850edae43bf8130504212f41c8f0828713abf3ae
Adetia Marhadi <adetia.marhadi@gmail.com>
2021-07-26 16:25:52 +0700

    create project from https://start.spring.io/

---
+++ src/main/java/com/github/adetiamarhadi/kafkaspringbootexample/KafkaSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.kafkaspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringbootExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/adetiamarhadi/kafkaspringbootexample/KafkaSpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.kafkaspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringbootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
